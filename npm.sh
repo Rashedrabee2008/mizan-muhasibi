16af0f188b40f7df8e071522ef2e8ce125074042
Rashedrabee2008 <126898391+Rashedrabee2008@users.noreply.github.com>
2026-08-28 16:44:59 +0300

    Add files via upload

---
+++ npm.sh
@@ -0,0 +1,15 @@
+# 1. إنشاء مجلد المشروع
+mkdir mizan-muhasibi
+cd mizan-muhasibi
+
+# 2. تهيئة المشروع
+npm init -y
+
+# 3. تثبيت Firebase
+npm install firebase
+
+# 4. تثبيت خادم محلي (اختياري)
+npm install -g live-server
+
+# 5. تشغيل التطبيق
+live-server
